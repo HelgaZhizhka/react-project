57cf505f6c8a88897fbf5844e4845021204b99f9
HelgaZhizhka <helgazhyzhka@gmail.com>
2023-11-28 13:30:33 +0100

    fix: fix search reset

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,20 +1,20 @@
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 
-import { currentPage, defaultPerPage } from '@/lib/types/constants';
+import { defaultPerPage } from '@/lib/types/constants';
 import { Button } from '@/components/Button';
 import styles from './Search.module.scss';
 
 const Search: React.FC = () => {
   const router = useRouter();
-  const { query = '', page = currentPage, per_page = defaultPerPage } = router.query;
+  const { query = '', per_page = defaultPerPage } = router.query;
   const [searchValue, setSearchValue] = useState(query);
 
   const handleSearch = () => {
     if (searchValue) {
-      router.push(`/?query=${searchValue}&page=${page}&per_page=${per_page}`);
+      router.push(`/?query=${searchValue}&page=1&per_page=${per_page}`);
     } else {
-      router.push(`/?page=${page}&per_page=${per_page}`);
+      router.push(`/?page=1&per_page=${per_page}`);
     }
     setSearchValue('');
   };
